ab1fcd2c25cd1d26295d1d8f4d48b611d81e7251
sandro luis de almeida junior <sandroluisjr06@gmail.com>
2024-06-25 17:27:20 -0300

    Update clubes.js

--- clubes.js
+++ clubes.js
@@ -13,7 +13,7 @@ menu()
    2- listar clubes
     3- editar clubes
     4- registrar partida
-    5- estasticas dos jogadores
+    5- artilheiro do time
     6- tabela `)
     
     rl.question('Escolha uma opção: ', (opcao) => {
@@ -70,3 +70,69 @@ function listar() {
     menu()
     }
 }
+    function editar(){
+        if (times.length == 0) {
+            console.log("Ainda não há time cadastrados.")
+            menu()
+        }else {
+            console.log('listar dastro ')
+            times.forEach((times, index) => {
+                console.log(`${index +1}. ${times.nome}`)
+            })
+            
+            rl.question ("Digite o numero do time que deseja editar.", (numero) => {
+                if (numero > 0 && numero <= times.length) {
+                    rl.question('Digite o nome do clube:' , (nome) => {
+                        rl.question('Digite a colocação:', (valor) =>{
+                            times[numero - 1] = {
+                                nome, 
+                                valor
+                            }
+                            console.log("times editado com sucesso!")
+                            menu()
+                        })
+                    })
+                }else {
+                    console.log('times inválido, tente novamente.')
+                    menu()
+                }
+        })
+    }
+    }
+    function registrar() {
+        if (times.length === 0) {
+            console.log("Ainda não há times cadastrados.");
+            menu();
+        } else {
+            console.log('Lista de times cadastrados:');
+            times.forEach((time, index) => {
+                console.log(`${index + 1}. ${time.nome}`);
+            });
+    
+            rl.question("Digite o número do time que venceu: ", (numeroVencedor) => {
+                let vencedorIndex = parseInt(numeroVencedor) - 1;
+    
+                rl.question("Digite o número do time que perdeu: ", (numeroPerdedor) => {
+                    let perdedorIndex = parseInt(numeroPerdedor) - 1;
+    
+                    if (vencedorIndex < 0 || vencedorIndex >= times.length || perdedorIndex < 0 || perdedorIndex >= times.length) {
+                        console.log("Número de time inválido.");
+                    } else {
+                        let timeVencedor = times[vencedorIndex];
+                        let timePerdedor = times[perdedorIndex];
+    
+                        console.log(`Partida registrada:\n${timeVencedor.nome} venceu ${timePerdedor.nome}`);
+                        
+                    }
+    
+                    readline
+                    menu();
+                });
+            });
+        }
+    }
+    
+   
+    
+   
+    
