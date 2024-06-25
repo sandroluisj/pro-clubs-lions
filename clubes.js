247d299c99c5afcf03107b98053d7ef11f739bbd
sandro luis de almeida junior <sandroluisjr06@gmail.com>
2024-06-25 16:31:28 -0300

    Update clubes.js

--- clubes.js
+++ clubes.js
@@ -1,1 +1,52 @@
+const readline = require('readline')
 
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+let times = []
+menu()
+
+    function menu(){
+ console.log(`
+   1-cadastrar clube
+   2- listar clubes
+    3- editar clubes
+    4- registrar partida
+    5- estasticas dos jogadores
+    6- tabela `)
+    
+    rl.question('Escolha uma opção: ', (opcao) => {
+        switch(opcao){
+            case "1":
+            cadastrar()
+            break
+
+            case "2": 
+            listar()
+            break
+
+            case "3":
+            editar()
+            break
+
+            case "4": 
+            registrar()
+            break
+
+            case "5": 
+            estastistica()
+            break
+            
+            case "6": 
+            tabela()
+            break
+            
+
+            default:
+                console.log("Opcão inválida.")
+                menu()
+                break                
+        }
+        })
+} 
